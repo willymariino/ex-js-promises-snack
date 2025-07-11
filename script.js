c35c13c0b5e001d36a23376da023fccce6e8076e
willymariino <wg.mariino@gmail.com>
2025-07-11 12:12:49 +0200

    created function and imported axios

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+🏆 Snack 1
+Ottieni il titolo di un post con una Promise.
+
+Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise
+ che recupera il titolo di un post dal link https://dummyjson.com/posts/{id}
+*/
+
+function getPostTitle(id) {
+    return new Promise((resolve, reject) => {
+
+    })
+}
