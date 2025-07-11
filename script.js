0aeab7b9f3c52e68ae336ffdcbd7d501bf4088eb
willymariino <wg.mariino@gmail.com>
2025-07-11 14:05:41 +0200

    snack 1 promises

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ Crea una funzione getPostTitle(id) che accetta un id e restituisce una Promise
 
 function getPostTitle(id) {
     return new Promise((resolve, reject) => {
-
+        axios.get(`https://dummyjson.com/posts/${id}`)
+            .then(res => resolve(res.data))
+            .catch(reject)
+            .finally(() => console.log("Richiesta completata"));
     })
 }
+
+getPostTitle(2)
+    .then(res => console.log(res))
+    .catch(error => console.error(error))
